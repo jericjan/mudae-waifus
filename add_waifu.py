97a9f2627247cea6f9b8d241bc94998b53aacb7c
jericjan <cantilfrederick@gmail.com>
2024-07-27 16:29:38 +0800

    add_waifu.py QoL update

--- add_waifu.py
+++ add_waifu.py
@@ -1,36 +1,105 @@
-import re
-from pathlib import Path
 import os
+import re
+import subprocess
+import webbrowser
+from typing import Any
+
+import pyperclip  # type: ignore
+from InquirerPy.base.control import Choice
+from InquirerPy.resolver import prompt
+
+
+def do_popen(coms: list[str]):
+    return subprocess.Popen(coms, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+
+def open_url(url: str):
+    webbrowser.register(
+        "chrome",
+        None,
+        webbrowser.BackgroundBrowser(
+            "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"
+        ),
+    )
+    webbrowser.get("chrome").open(url)
+
+
+def select_options(msg: str, choices: list[Any], type: str = "list", default: str = ""):
+    return {
+        "type": type,
+        "message": msg,
+        "choices": choices,
+        "default": default,
+        "max_height": "70%",
+    }
+
+
+hugo_proc = do_popen(["hugo", "serve", "--quiet"])
 
 name = input("Enter waifu name: ")
 img = input("Enter waifu image url: ")
 
 
-hugo_str = f"\n{{{{< gallery src=\"{img}\" title=\"{name}\" >}}}}"
+accs = ["kur0", "biggus", "lemonika"]
 
-accs = ['kur0.md', 'biggus.md', 'lemonika.md']
+questions = [select_options("Select acc:", accs)]
 
-posts_folder = Path('content/posts/')
+selected_acc: str = prompt(questions=questions)[0]  # type: ignore
 
-for idx, file in enumerate(accs):
-    print(f"{idx+1}: {file}")
+open_url(f"http://localhost:1313/posts/{selected_acc}/")
 
-while True:
-    try:
-        choice = int(input("Pick an acc number: "))
-    except ValueError:
-        print("Not a number")
-        continue
-    if (1 <= choice <= len(accs)):
-        choice -= 1
-        break
-    print("Invalid!")
+name_reg = re.compile(r"title=\"(.+?)(?:\||\")")
 
-target_file = posts_folder / accs[choice]
+with open("content/posts/kur0.md", encoding="utf-8") as f:
+    txt = f.readlines()
 
-with open(target_file, "a", encoding="utf-8", newline='') as f:
-    f.write(hugo_str)
+all_names = name_reg.findall("".join(txt))
 
-os.system(f"neovide --opengl {str(target_file.absolute())}")
+questions = [
+    select_options("Select waifu to place before/after:", all_names, "fuzzy"),
+    select_options(
+        "Select direction to insert in:",
+        [Choice(value=0, name="above"), Choice(value=1, name="below")],
+        default="above",
+    ),
+]
+result = prompt(questions=questions)
 
-        
+target = result[0]
+offset: int = result[1]  # type: ignore
+
+hugo_str = f'{{{{< gallery src="{img}" title="{name}" >}}}}\n'
+
+for line_no, line in enumerate(txt):
+    match = name_reg.search(line)
+    if match:
+        if match.group(1) == target:
+            txt.insert(line_no + offset, hugo_str)
+            break
+
+if offset == 0:  # Above
+    smp = f"$smp {name} $ {target}"
+else:  # Below
+    smp = f"$smp {target} $ {name}"
+
+with open("content/posts/kur0.md", "w", encoding="utf-8", newline="") as f:
+    f.write("".join(txt))
+
+pyperclip.copy(smp)  # type: ignore
+print(f"Your $smp command is: {smp}\nIt has been copied to the clipboard.\n")
+input("Press enter to kill the hugo server and build the static site.\n")
+
+hugo_proc.kill()
+
+os.system("hugo")
+
+os.system("git status")
+
+questions = [
+    select_options("Do you also want to commit and push?", ['yes', 'no']),
+]
+result = prompt(questions=questions)[0]
+
+if result == "yes":
+    os.system("git commit -m '.'")
+    os.system("git push origin main")
